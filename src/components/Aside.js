68a56519a925bfedf47718ce965fef366074e509
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-10-16 20:11:08 +0530

    init

---
+++ src/components/Aside.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Lodash from 'lodash'
+import { products } from '../data.json';
+class Aside extends React.Component {
+  render() {
+   let sizes = products.reduce((acc,cv)=>{
+      acc = acc.concat(cv.availableSizes)
+      return acc
+    },[])
+    let uniqSizes = Lodash.uniq(sizes);
+    let { activeSizes } = this.props.state;
+    return (
+      <section className="flex-20 mr-2">
+        <h3 className="font-medium text-lg">Sizes:</h3>
+        <div className="flex flex-wrap mt-6">
+          {uniqSizes.map((size, index) => (
+            <div key={index}>
+              <label
+                htmlFor={size}
+                className={
+                  activeSizes.indexOf(size) !== -1
+                    ? 'rounded-full inline-block text-xs font-light w-9 h-9 leading-9 text-center mr-2 mb-4 bg-black text-white'
+                    : 'rounded-full inline-block text-xs font-light bg-gray-200 w-9 h-9 leading-9 text-center mr-2 mb-4 hover:bg-gray-400'
+                }
+              >
+                {size}
+              </label>
+              <input
+                type="checkbox"
+                id={size}
+                name={size + index}
+                value={size}
+                className="border hidden"
+                onChange={this.props.handleSortBySize}
+              />
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Aside;
